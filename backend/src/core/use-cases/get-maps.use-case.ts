186e1e214fe065f8b8b9c8a734e3f9569d2643b6
maxamills <morrm046@gmail.com>
2025-06-30 18:45:37 +0100

    Fix return types

--- backend/src/core/use-cases/get-maps.use-case.ts
+++ backend/src/core/use-cases/get-maps.use-case.ts
@@ -3,6 +3,7 @@ import {IMapQueries} from '@shared/interfaces/queries';
 import {fetchApiResource} from '../../infrastructure/services/fetch-api.service';
 import {ValorantMapsMapper} from '../mappers/valorantMaps.mapper';
 import {MapDto} from '@shared/interfaces/dto/map.dto';
+import { IMap } from '@shared/interfaces';
 
 @Injectable()
 export class GetMapsUseCase {
@@ -10,7 +11,7 @@ export class GetMapsUseCase {
 
   constructor(private readonly valMapMapper: ValorantMapsMapper) {}
 
-  public async execute({siteCount}: IMapQueries): Promise<void> {
+  public async execute({siteCount}: IMapQueries): Promise<IMap[]> {
     const data = await fetchApiResource<MapDto[]>('maps');
 
     return this.valMapMapper.mapValorantMaps({siteCount}, data);
--- backend/src/infrastructure/controllers/maps.controller.ts
+++ backend/src/infrastructure/controllers/maps.controller.ts
@@ -1,5 +1,6 @@
 import {Controller, Get, Param, Query} from '@nestjs/common';
 import {GetMapsUseCase} from '../../core/use-cases/get-maps.use-case';
+import { IMap } from '@shared/interfaces';
 
 @Controller({
   version: '1',
@@ -9,7 +10,7 @@ export class MapsController {
   constructor(private readonly getMapsUseCase: GetMapsUseCase) {}
 
   @Get()
-  getAllMaps(@Query('sites') sites: string): Promise<void> {
+  getAllMaps(@Query('sites') sites: string): Promise<IMap[]> {
     try {
       const siteCount = sites ? parseInt(sites, 10) : null;
       return this.getMapsUseCase.execute({siteCount});
